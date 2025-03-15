3d809f9d6d48b2ed7b24e18255ed4d0ad139eb4e
balajivk5007 <vkbalaji1998.gmail.com>
2025-03-15 12:45:24 +0530

    thirdmessage

--- work.js
+++ work.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.send('April');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+console.log("Thanks you")
